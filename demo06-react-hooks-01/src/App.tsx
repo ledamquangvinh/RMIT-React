15255108698eacaad058efca3480e2cb15f8942f
Le Dam Quang Vinh <s3818480@rmit.edu.vn>
2023-09-12 10:18:53 +0700

    feat: React hook 1 finished

---
+++ demo06-react-hooks-01/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.sass'
+import Button from './components/Button'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <Button disable={false} title="I'm a button"></Button>
+    </div>
+  )
+}
+
+export default App
---
+++ demo06-react-hooks-01/src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+interface Prop {
+  title   : string
+  disable : boolean
+}
+
+const Button : React.FC<Prop> = ({title, disable}: Prop) => {
+  return (
+    <button disabled={disable}>{title}</button>
+  )
+}
+
+export default Button
